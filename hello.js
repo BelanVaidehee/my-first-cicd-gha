f2cec5d5047a01f644d4f425a02f76422ea055ab
belanvaidehee@gmail.com <belanvaidehee@gmail.com>
2025-06-14 14:56:52 +0530

    added username password

--- hello.js
+++ hello.js
@@ -1,8 +1,15 @@
-console.log("Hello, World!");
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = 3000;
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'dbuser',
-    password: 'vaidehee',
-    database: 'your_database'
-  });
+/*const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'testdb'
+});*/
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
