ac0430410fa136e7241e3a78909e739dffe17dd6
belanvaidehee@gmail.com <belanvaidehee@gmail.com>
2025-06-14 14:11:22 +0530

    added username password

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
 console.log("Hello, World!");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'dbuser',
+    password: 'vaidehee',
+    database: 'your_database'
+  });
