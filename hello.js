ca810caaa9bdf6f04b3d74e95358cac45eab36fc
belanvaidehee@gmail.com <belanvaidehee@gmail.com>
2025-06-14 15:00:35 +0530

    added username pass

--- hello.js
+++ hello.js
@@ -3,12 +3,12 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-/*const connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
   database: 'testdb'
-});*/
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
