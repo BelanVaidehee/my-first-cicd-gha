1612c011960517aa414c7c914d32c568d87147a0
belanvaidehee@gmail.com <belanvaidehee@gmail.com>
2025-06-15 22:54:05 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const port = 3000;
 
 // Vulnerable: Hardcoded credentials
 const connection = mysql.createConnection({
